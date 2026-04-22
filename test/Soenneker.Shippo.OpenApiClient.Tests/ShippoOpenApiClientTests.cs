4712d03d866809b139458916a294e9051ad8c98e
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:03:37 -0500

    TUnit conversion

--- test/Soenneker.Shippo.OpenApiClient.Tests/ShippoOpenApiClientTests.cs
+++ test/Soenneker.Shippo.OpenApiClient.Tests/ShippoOpenApiClientTests.cs
@@ -1,16 +1,15 @@
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Shippo.OpenApiClient.Tests;
 
-[Collection("Collection")]
-public sealed class ShippoOpenApiClientTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class ShippoOpenApiClientTests : HostedUnitTest
 {
-    public ShippoOpenApiClientTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public ShippoOpenApiClientTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
